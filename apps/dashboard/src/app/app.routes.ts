4651a15b547442e8cba7a4cc62b18dae7e8e4357
Hien Nguyen The <kinhcan853@gmail.com>
2026-01-11 21:33:22 +0700

    add todo list app

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,9 +1,12 @@
-import { NxWelcome } from './nx-welcome';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'todo_list',
+    loadChildren: () => import('todo_list/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
-    component: NxWelcome,
+    loadChildren: () => import('todo_list/Routes').then((m) => m!.remoteRoutes),
   },
 ];
