c786e71bd2fbd506fe0dd3a5b15554156ca61196
hien.nguyenthe <kinhcan853@gmail.com>
2026-01-12 12:04:12 +0700

    add todo creation

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'todo_creation',
+    loadChildren: () =>
+      import('todo_creation/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'todo_list',
     loadChildren: () => import('todo_list/Routes').then((m) => m!.remoteRoutes),
   },
