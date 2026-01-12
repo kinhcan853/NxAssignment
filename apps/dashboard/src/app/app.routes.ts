9939712e68e0658602863d023f89896adbc0dd2c
hien.nguyenthe <kinhcan853@gmail.com>
2026-01-12 17:22:37 +0700

    add statistic app

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'statistic',
+    loadChildren: () => import('statistic/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'about',
     loadChildren: () => import('about/Routes').then((m) => m!.remoteRoutes),
   },
